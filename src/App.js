44b22723e847015a69964f2ae281a2d39f0a8b38
Alex Berns <eberron94@gmail.com>
2021-04-05 18:09:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { CardGenerator, CardPrinter } from './components';
 
 function App() {
-    // const [printMode, setPrintMode] = useState(false);
-    // const [currHash, setCurrHash] = useState('');
-
-    // useEffect(() => {
-    //     console.log('current hash', window.location.hash);
-    //     setCurrHash(window.location.hash);
-    // },[window.location.hash]);
-
-    // useEffect(() => {
-    //     window.addEventListener('hashchange', window.location.reload(), false);
-    //     // return () =>
-    //     //     window.removeEventListener(
-    //     //         'hashchange',
-    //     //         window.location.reload(),
-    //     //         false
-    //     //     );
-    // }, []);
-
     return (
         <Switch>
             <Route exact path='/'>
